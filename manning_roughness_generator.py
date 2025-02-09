d43bacb6e81aef2589e790d12ce645588872ffe1
mabdazzam <mhmdabdullahazzam@gmail.com>
2025-02-09 01:44:10 -0700

    add header and licensing info

--- manning_roughness_generator.py
+++ manning_roughness_generator.py
@@ -1,5 +1,33 @@
 # -*- coding: utf-8 -*-
 
+"""
+Manning's Roughness Generator - A QGIS Plugin
+Generates Manning's roughness coefficient layers for hydrological modeling.
+
+Created on: 2025-02-08
+Copyright: (C) 2025 by Abdullah Azzam
+Email: mabdazzam@outlook.com
+
+This program is free software: you can redistribute it and/or modify it
+under the terms of the GNU General Public License as published by the
+Free Software Foundation, either version 3 of the License, or (at your
+option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program. If not, see <https://www.gnu.org/licenses/>.
+"""
+
+__author__ = "Abdullah Azzam"
+__date__ = "2025-02-08"
+__copyright__ = "(C) 2025 by Abdullah Azzam"
+
+# -*- coding: utf-8 -*-
+
 
 from qgis.PyQt.QtCore import QCoreApplication
 from qgis.PyQt.QtGui import QIcon
